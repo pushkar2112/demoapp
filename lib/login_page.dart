d341c66cda6b48bb7e32d7f449550e4eee1b8091
Pushkar Verma <pushkar7397@gmail.com>
2024-01-27 11:19:47 +0530

    Add Login Page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,30 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+final TextEditingController emailController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();
+
+bool validateForm(){
+  if (!EmailValidator.validate(emailController.text)) {
+    print("Wrong Email");
+    return false;
+  } else if (passwordController.text.length < 6) {
+    print("Wrong Password");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+bool loginFunc(){
+  if (validateForm()) {
+    print("Login Success");
+    return true;
+  } else {
+    return false;
+  }
+}
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -10,6 +36,41 @@ class LoginPage extends StatelessWidget {
         title: Text("Login", style: TextStyle(color: Colors.white),),
         centerTitle: true,
         backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 30, right: 30),
+          children: [
+            Lottie.asset("assets/lottie/login_animation.json"),
+            Container(
+              height: 30,
+            ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Email",
+              ),
+            ),
+            Container(
+              height: 30,
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Password",
+              ),
+            ),
+            Container(
+              height: 30,
+            ),
+            ElevatedButton(onPressed: (){
+              loginFunc();
+            }, child: Text("Login Now"))
+          ],
+        ),
       ),
     );
   }
