3e10541ca6a9fd635e898728e553f4fb0251082e
Pushkar Verma <pushkar7397@gmail.com>
2024-01-27 10:17:15 +0530

    Set AppBar for LoginPage

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,6 +5,12 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login", style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+    );
   }
 }
